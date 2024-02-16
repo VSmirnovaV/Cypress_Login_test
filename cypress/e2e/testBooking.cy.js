e8ac9e0c18f2c9873c6728ea150d020b08096e02
VSmirnovaV <131758107+VSmirnovaV@users.noreply.github.com>
2024-02-16 14:05:52 +0300

    corrected fixtures, add test

--- cypress/e2e/testBooking.cy.js
+++ cypress/e2e/testBooking.cy.js
@@ -1,61 +1,122 @@
 describe('test home page elements display visible', () => {
-  beforeEach(() => {
-    cy.open('/', 'Идём');
-  });
+	beforeEach(() => {
+		cy.open('/', 'Идём');
+	});
 
-  it('test home page display', () => {
-    cy.visibleTitle();
-  });
+	it('test home page display', () => {
+		cy.visibleTitle();
+	});
 
-  it('test be visible name films', () => {
-    cy.fixture('selector').then(sel => {
-      cy.get(sel.film_name_1).should('be.visible');
-      cy.get(sel.film_name_2).should('be.visible');
-      cy.get(sel.film_name_3).should('be.visible');
-    })
-  });
+	it('test be visible name films', () => {
+		cy.fixture('selector').then(sel => {
+			cy.get(sel.film_name_1).should('be.visible');
+			cy.get(sel.film_name_2).should('be.visible');
+			cy.get(sel.film_name_3).should('be.visible');
+		})
+	});
 
-  it('test be visible days of the week', () => {
-    cy.visibleText('Пн');
-    cy.visibleText('Вт');
-    cy.visibleText('Ср');
-    cy.visibleText('Чт');
-    cy.visibleText('Пт');
-    cy.visibleText('Сб');
-    cy.visibleText('Вс');
-  })
+	it('test be visible days of the week', () => {
+		cy.visibleText('Пн');
+		cy.visibleText('Вт');
+		cy.visibleText('Ср');
+		cy.visibleText('Чт');
+		cy.visibleText('Пт');
+		cy.visibleText('Сб');
+		cy.visibleText('Вс');
+	})
 });
 
 describe('test admin', () => {
-  it('successful authorization as administrator', () => {
-    cy.authorization('user', 'Управление залами');
-    });
-  
-  it('unsuccessful authorization as administrator with wrong data', () => {
-    cy.cycle('wrong_data', 'Ошибка авторизации');
-    });
+	it('successful authorization as administrator', () => {
+		cy.fixture('user_data').then(function (regdata) {
+			this.regdata = regdata;
+			cy.login(regdata.validEmail, regdata.validPassword);
+			cy.visibleText('Управление залами');
+		})
+	})
 
-  it('unsuccessful authorization as administrator with empty field', () => {
-    cy.cycle('empty_field', 'Заполните это поле.')
-  })
-});
+
+	it('unsuccessful authorization as administrator with invalid email', () => {
+		cy.fixture('user_data').then(function (regdata) {
+			this.regdata = regdata;
+			cy.login(regdata.invalidEmail, regdata.validPassword);
+			cy.visibleText('Ошибка авторизации');
+		})
+	})
+
+	it('unsuccessful authorization as administrator with invalid password', () => {
+		cy.fixture('user_data').then(function (regdata) {
+			this.regdata = regdata;
+			cy.login(regdata.validEmail, regdata.invalidEmail);
+			cy.visibleText('Ошибка авторизации');
+		})
+	})
+
+	it('unsuccessful authorization as administrator with invalid password and email', () => {
+		cy.fixture('user_data').then(function (regdata) {
+			this.regdata = regdata;
+			cy.login(regdata.invalidEmail, regdata.invalidPassword);
+			cy.visibleText('Ошибка авторизации');
+		})
+	})
+
+	it('unsuccessful authorization as administrator with empty field email', () => {
+		cy.fixture('user_data').then(function (regdata) {
+			this.regdata = regdata;
+			cy.login(regdata.emptyFieldEmail, regdata.validPassword);
+			cy.fixture('selector').then(sel => {
+				cy.get(sel.login).then((elements) => {
+					expect(elements[0].checkValidity()).to.be.false
+					expect(elements[0].validationMessage).to.be.eql('Заполните это поле.');
+				})
+			})
+		})
+	})
+
+	it('unsuccessful authorization as administrator with empty field password', () => {
+		cy.fixture('user_data').then(function (regdata) {
+			this.regdata = regdata;
+			cy.login(regdata.validEmail, regdata.emptyFieldPass);
+			cy.fixture('selector').then(sel => {
+				cy.get(sel.pass).then((elements) => {
+					expect(elements[0].checkValidity()).to.be.false
+					expect(elements[0].validationMessage).to.be.eql('Заполните это поле.');
+				})
+			})
+		})
+	})
+
+	it('unsuccessful authorization as administrator with empty field password and email', () => {
+		cy.fixture('user_data').then(function (regdata) {
+			this.regdata = regdata;
+			cy.login(regdata.emptyFieldEmail, regdata.emptyFieldPass);
+			cy.fixture('selector').then(sel => {
+				cy.get(sel.login).then((elements) => {
+					expect(elements[0].checkValidity()).to.be.false
+					expect(elements[0].validationMessage).to.be.eql('Заполните это поле.');
+				})
+			})
+		})
+	})
+})
+
 
 describe('booking a film whose title was received from the admin', () => {
-  it('successfully getting the movie title from the admin', () => {
-    cy.authorization('user', 'Управление залами');
-    cy.fixture('selector').then(sel => {
-      cy.get(sel.film_name_admin).then(($el) => $el.textContent).should('have.text', 'Зверополис');
-      cy.get(sel.film_name_admin).invoke('text').then((text) => {
-        cy.open('/', 'Идём');
-        cy.clickElement(sel.day_3);
-        cy.get(sel.film_name_1).should('have.text', text);
-        cy.clickElement(sel.time);
-        cy.clickElement(sel.seat_1);
-        cy.clickElement(sel.seat_2);
-        cy.clickElement(sel.button_booking)
-        cy.visibleText('Получить код бронирования')
-      })
-    })
-  })
+	it('successfully getting the movie title from the admin', () => {
+		cy.authorization('user', 'Управление залами');
+		cy.fixture('selector').then(sel => {
+			cy.get(sel.film_name_admin).then(($el) => $el.textContent).should('have.text', 'Зверополис');
+			cy.get(sel.film_name_admin).invoke('text').then((text) => {
+				cy.open('/', 'Идём');
+				cy.clickElement(sel.day_3);
+				cy.get(sel.film_name_1).should('have.text', text);
+				cy.clickElement(sel.time);
+				cy.clickElement(sel.seat_1);
+				cy.clickElement(sel.seat_2);
+				cy.clickElement(sel.button_booking)
+				cy.visibleText('Получить код бронирования')
+			})
+		})
+	})
 })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -17,54 +17,13 @@ Cypress.Commands.add('open', (link, text) => {
 	cy.contains(text).should('be.visible');
 });
 
-Cypress.Commands.add('authorization', (fixture, text) => {
-	cy.fixture(fixture).then(data => {
+Cypress.Commands.add('login', (email, password) => {
 		cy.open('https://qamid.tmweb.ru/admin/', 'Администраторррская');
-			const email = data.email;
-			const password = data.password;
 			cy.fixture('selector').then(sel => {
+				if(email)
 				cy.get(sel.login).type(email);
+				if(password)
 				cy.get(sel.pass).type(password);
 				cy.get(sel.button).click();
-				cy.visibleText(text);
 		})
-	})
-});
-
-Cypress.Commands.add('cycle', (fixture, text) => {
-	cy.fixture(fixture).its('data').then(data => {
-		data.forEach((data) => {
-			cy.open('https://qamid.tmweb.ru/admin/', 'Администраторррская');
-			const email = data.email;
-			const password = data.password;
-			cy.fixture('selector').then(sel => {
-				if (password != null && email == null) {
-					cy.get(sel.pass).type(password);
-					cy.get(sel.button).click();
-					cy.get(sel.login).then((elements) => {
-						expect(elements[0].checkValidity()).to.be.false
-						expect(elements[0].validationMessage).to.be.eql(text);
-					});
-				} else if (email != null && password == null) {
-					cy.get(sel.login).type(email);
-					cy.get(sel.button).click();
-					cy.get(sel.pass).then((elements) => {
-						expect(elements[0].checkValidity()).to.be.false
-						expect(elements[0].validationMessage).to.be.eql(text);
-					});
-				} else if (email != null && password != null) {
-					cy.get(sel.login).type(email);
-					cy.get(sel.pass).type(password);
-					cy.get(sel.button).click();
-					cy.visibleText(text);
-				} else {
-					cy.get(sel.button).click();
-					cy.get(sel.login).then((elements) => {
-						expect(elements[0].checkValidity()).to.be.false
-						expect(elements[0].validationMessage).to.be.eql(text);
-					});
-				}
-			})
-		})
-	})
 })
